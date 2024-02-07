6a6fd2979a7bfa15d00d85707816914257c5b643
Ikonnikov Oleg <Ikonnikov.oleg47>
2024-02-07 20:37:26 +0500

    like a button

--- tests/test_like_a_button_page.py
+++ tests/test_like_a_button_page.py
@@ -1,10 +1,12 @@
-from selenium.webdriver.common.by import By
+from pages.like_a_button import LikeAButtonPage
 
 def test_button2_exist(browser):
-    browser.get('https://www.qa-practice.com/elements/button/like_a_button')
-    assert browser.find_element(By.LINK_TEXT, 'Click').is_displayed()
+    like_a_button = LikeAButtonPage(browser)
+    like_a_button.open()
+    assert like_a_button.button_is_displayed
 
 def test_button2_clicked(browser):
-    browser.get('https://www.qa-practice.com/elements/button/like_a_button')
-    browser.find_element(By.LINK_TEXT, 'Click').click()
-    assert 'Submitted' == browser.find_element(By.ID, 'result-text').text
+    like_a_button = LikeAButtonPage(browser)
+    like_a_button.open()
+    like_a_button.button_click()
+    assert 'Submitted' == like_a_button.result_text
